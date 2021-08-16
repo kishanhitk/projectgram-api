6098735a029e349d12f3f2a56c64ac41b65f09d4
kishanhitk <kishansharma1231@gmail.com>
2021-08-16 14:34:45 +0530

    feat: Check existing user with mail

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -20,12 +20,19 @@ export class UsersService {
 
     if (user.password.length < 8)
       throw new BadRequestException('Password must be of minimum 8 characters');
-    // Check if user already exists
+    // Check if user already exists with username
     const existingUser = await this.userRepository.findOne({
       username: user.username,
     });
     if (existingUser) {
       throw new ConflictException('This username is already taken');
+    }
+    // CHeck if user already exists with email
+    const existingUserByEmail = await this.userRepository.findOne({
+      email: user.email,
+    });
+    if (existingUserByEmail) {
+      throw new ConflictException('This email is already taken');
     }
 
     const newUser = new User();
