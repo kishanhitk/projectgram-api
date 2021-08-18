a6b1bd06f11994d7bfa1c016fbe0117387997ec1
kishanhitk <kishansharma1231@gmail.com>
2021-08-18 13:27:24 +0530

    feat: Return access_token with user details on login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,8 +24,8 @@ export class AuthService {
   async login(user: User) {
     const payload = { username: user.username, sub: user.id };
     return {
-      user: user,
       access_token: this.jwtService.sign(payload),
+      ...user,
     };
   }
 }
